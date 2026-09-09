6f7de9d474631e0dd8e22280ab88070e8451e3d0
Gabriel Guerra <gabrielpessoaguerracavalcanti@gmail.com>
2026-09-09 19:38:34 -0300

    navbar hover

--- components/layout/navbar.tsx
+++ components/layout/navbar.tsx
@@ -1,39 +1,73 @@
 import Link from "next/link";
 
+const linkClassName =
+    "relative pb-1 after:absolute after:bottom-0 after:left-0 after:h-px after:w-full after:origin-left after:scale-x-0 after:bg-black after:transition-transform after:duration-300 after:ease-out hover:after:scale-x-100";
+
 export function Navbar() {
     return (
         <header className="border-b border-black">
             <div className="flex items-center justify-between px-6 py-4">
-                <Link href="/" className="text-xl font-bold uppercase">
+                <Link
+                    href="/"
+                    className="text-xl font-bold uppercase"
+                >
                     Casa Elefante
                 </Link>
 
                 <nav>
                     <ul className="flex gap-6">
                         <li>
-                            <Link href="/">Loja</Link>
+                            <Link
+                                href="/"
+                                className={linkClassName}
+                            >
+                                Loja
+                            </Link>
                         </li>
 
                         <li>
-                            <Link href="/toda-terca-tem">
+                            <Link
+                                href="/toda-terca-tem"
+                                className={linkClassName}
+                            >
                                 Toda Terça Tem
                             </Link>
                         </li>
 
                         <li>
-                            <Link href="/sobre">Sobre</Link>
+                            <Link
+                                href="/sobre"
+                                className={linkClassName}
+                            >
+                                Sobre
+                            </Link>
                         </li>
 
                         <li>
-                            <Link href="/contato">Contato</Link>
+                            <Link
+                                href="/contato"
+                                className={linkClassName}
+                            >
+                                Contato
+                            </Link>
                         </li>
 
                         <li>
-                            <Link href="/newsletter">Newsletter</Link>
+                            <Link
+                                href="/newsletter"
+                                className={linkClassName}
+                            >
+                                Newsletter
+                            </Link>
                         </li>
 
                         <li>
-                            <Link href="/carrinho">Carrinho</Link>
+                            <Link
+                                href="/carrinho"
+                                className={linkClassName}
+                            >
+                                Carrinho
+                            </Link>
                         </li>
                     </ul>
                 </nav>
